2748b24e475f5fc5bf3ec087b188f775481655a5
FabianaSoft <teixeira.fabiana1@gmail.com>
2024-05-02 09:26:56 -0300

    inserir config no css

--- gerenciaDeNomes.js
+++ gerenciaDeNomes.js
@@ -1,6 +1,6 @@
-function concatena(){
+function concatenar(){
     var nome = document.getElementById("nome").value;
     var sobrenome = document.getElementById("sobrenome").value;
-    var nomeCompleto = nome + " " + sobrenome;
+    var nomeCompleto = nome + "<br>" + sobrenome;
     document.getElementById("nomeCompleto").innerHTML = nomeCompleto;
 }
